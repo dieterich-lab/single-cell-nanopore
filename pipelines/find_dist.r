62ecbff9d6cb4305c354b1ab569cee99ac66b435
qwang-big <qwang.big@gmail.com>
2020-04-28 09:47:35 +0200

    Update find_dist.r

--- pipelines/find_dist.r
+++ pipelines/find_dist.r
@@ -4,5 +4,7 @@ Sys.setlocale("LC_NUMERIC","C")
 args = commandArgs(trailingOnly=TRUE)
 x=read.table(args[1])
 y=read.table(args[2])
-r=unlist(lapply(y[seq(2,nrow(y),2),1],function(d) x[stringdist(d,x[,2],method='dl')<2,2]))
+y=y[seq(2,nrow(y),2),1]
+r=unlist(lapply(y,function(d) x[stringdist(d,x[,2],method='dl')<2,2]))
+r=unique(c(r,y))
 write.table(r,file=args[3],sep="\t",quote=F,row.names=F,col.names=F)
