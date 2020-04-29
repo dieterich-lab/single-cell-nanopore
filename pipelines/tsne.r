bb97e79ca81f0ff760bd0bc229c6a7c3bf72661e
qwang-big <qwang.big@gmail.com>
2020-04-29 11:27:18 +0200

    Update tsne.r

--- pipelines/tsne.r
+++ pipelines/tsne.r
@@ -5,7 +5,8 @@ nm=read.table('FC1.ge')
 i=match(y$id,nm[,1])
 df=data.frame(y$barcode,nm[i,2])
 colnames(df)=c('barcode','gene')
-x=reshape2::dcast(df[!is.na(df[,2]),], gene~barcode, length)
+df=df[!is.na(df[,2]),]
+x=reshape2::dcast(df, gene~barcode, length)
 rownames(x)=x[,1]
 x=x[,-1]
 library(Rtsne)
@@ -15,7 +16,9 @@ lw=function(d) length(which(d))
 i=apply(x,1,function(d) lw(d>0))
 j=apply(x,2,function(d) lw(d>0))
 x=t(x[i>=3,j>=50])
+cb=read.table(gzfile("barcodes.tsv.gz"),sep='-')[,1]
+x=x[rownames(x) %in% cb,]
 tsne <- Rtsne(x, dims=2, perplexity=30, initial_dims=10, num_threads=0)
-png("gfp.png")
+png("FC1-gfp.png")
 plot(tsne$Y,col=c('black','red')[as.integer(rownames(x) %in% lbl)+1])
 dev.off()
