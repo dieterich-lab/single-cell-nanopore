e7c83a2e24d2280e80a2a6458fc58dfd9462be8e
qwang-big <qwang.big@gmail.com>
2020-06-12 17:09:03 +0200

    match new barcode field

--- pipelines/add_label.r
+++ pipelines/add_label.r
@@ -3,6 +3,6 @@ Sys.setlocale("LC_NUMERIC","C")
 args = commandArgs(trailingOnly=TRUE)
 x=read.table(args[1],sep="\t",header=TRUE)
 y=read.table(args[2],sep="\t",header=FALSE,row.names=1)
-x[,ncol(x)] = 1-as.integer(y[x[,1],1]==x[,2])
+x[,ncol(x)] = 1-as.integer(y[x[,1],2]==x[,2])
 colnames(x)[ncol(x)]='label'
 write.table(x,file=args[3],sep="\t",quote=F,row.names=FALSE)
