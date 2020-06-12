ef6ddfcc066d34161a7a51838e6d820e7f3488a6
qwang-big <qwang.big@gmail.com>
2020-06-12 14:09:23 +0200

    sam file with ground truth barcodes

---
+++ pipelines/fa2sam.pl
@@ -0,0 +1,23 @@
+$fa=shift;
+$tab=shift;
+$len=shift;
+$clen=shift;
+
+open(F,"<$tab");
+while(<F>){chomp;
+@t=split(/\t/);
+$h{$t[0]}="GE:Z:$t[1]\tXX:Z:$t[2]\tUU:Z:$t[3]"
+}
+close F;
+
+print '@SQ	SN:1	LN:100',"\n";
+open(F,"<$fa");
+while(<F>){chomp;
+$b=/^>/;
+$l=length($_);
+$l2=$l-$clen;
+$q='F'x$l;
+$s=substr($_,1) if $b;
+print "$s\t0\t1\t21\t31\t${l2}S${clen}M\t*\t0\t0\t$_\t$q\t$h{$s}\tXF:Z:CODING\tUQ:Z:$q\tUS:Z:$_\n" if $l==$len and !$b
+}
+close F
